9c986a096849b6081b2ed64866b0cb938415428c
YoyoXD <146937662+YoyoNinovD@users.noreply.github.com>
2024-01-19 23:55:49 +0200

    Update towns.js (added shuffle function)

--- towns.js
+++ towns.js
@@ -1,19 +1,22 @@
-$(document).ready(function() {
-	$('#btnDelete').click(deleteTown)
+document.addEventListener("DOMContentLoaded", function () {
+    document.getElementById("btnDelete").addEventListener("click", deleteTown);
 });
 
 function deleteTown() {
-	let townName = $('#townName').val();
-	$('#townName').val('');
-	let removed = false;
-	for (let option of $('#towns option')) {
-		if (option.textContent == townName) {
-			removed = true;
-			option.remove();
-		}
-	}
-	if (removed)
-		$('#result').text(townName + " deleted.");
-	else
-		$('#result').text(townName + " not found.");
+    let townName = document.getElementById("townName").value;
+    document.getElementById("townName").value = "";
+    let removed = false;
+    let options = document.querySelectorAll("#towns option");
+    for (let i = 0; i < options.length; i++) {
+        if (options[i].textContent == townName) {
+            removed = true;
+            options[i].remove();
+        }
+    }
+}
+function shuffleOptions() {
+    var select = document.getElementById('towns');
+    for (var i = select.children.length; i >= 0; i--) {
+        select.appendChild(select.children[Math.random() * i | 0]);
+    }
 }
